6a014f1005518f15b2d8848083cc32f2c81481a5
João Pedro Gomes <126845637+MrSutoly@users.noreply.github.com>
2025-12-20 01:13:32 -0300

    UPDATE: codigo aula 2

--- Card 2/video1_aula.py
+++ Card 2/video1_aula.py
@@ -8,6 +8,8 @@ import pacote.sub.arquivo_aula #importamos o arquivo de outra pasta, agora se a 
 print(__name__) #printa __main__ pois estamos no modulo main, independente do nome do arquivo
 print(__package__) #printa None porque nao estamos em nenhum pacote
 
+print(' ')
+
 # VARIAVEIS
 
 #define a como 3 e b como 4.4
@@ -15,6 +17,8 @@ a = 3
 b = 4.4
 
 print(a + b) #printa 7.4
+
+print(' ')
 
 #atribui valores a variaveis texto e idade
 texto = 'Sua idade e...'
@@ -24,15 +28,23 @@ idade = 23
 
 print(texto + str(idade)) #esse str(idade) converte de int para string assim podendo concatenar
 
+print(' ')
+
 print(f'{texto} {idade}') # esse f'' serve para interpolar valores, ler algo fora do texto e interpretar dentro do texto, se fizer uma soma de 12 + 3 o resultado da 15
 
-print( 3 * 'bom dia') #printa bom dia 3 vezes
+print(' ')
+
+print( 3 * 'bom dia ') #printa bom dia 3 vezes
 
 PI = 3.14 #PI em maiusculo e uma convencao para declarar constantes, pois em python nao existe constantes
+
+print(' ')
 
 raio = float(input('Informe o raio da circ?')) #pedido para o usuario informar via terminal um valor, e definimos a resposta como valor da variavel raio
 area = PI * pow(raio,2) #gera o resultado da area a partir do valor do raio que o usuario passou
 #a funcao pow(x,y) o x e a base e o y o expoente da potencia
+
+print(' ')
 
 print(f'A area da circ e {area} m2') #printa o resultado no terminal
 
@@ -154,7 +166,7 @@ res %= 4 #esta pegando o resto da divisao por 4 do valor atual de res
 
 b1 = True
 b2 = False
-b3 = True
+b3 = True #definindo variaveis booleanas
 
 print(b1 and b2 and b3) #printa falso pois para printar verdadeiro todos devem ser verdadeiro
 print(b1 or b2 or b3) #printa true pois basta 1 ser verdadeiro pra printar true
@@ -242,6 +254,8 @@ print('')
 for n in {1, 2, 3, 4, 4, 4}: #mesmo nao tendo indices da pra rodar um for num conjunto
     print(n, end = '')
     
+print('')
+    
 produto = {
     'nome': 'Caneta',
     'preco': 8.90,
@@ -261,23 +275,29 @@ print('')
 for val in produto.values(): #printa somente os valores de cada chave
     print(val, end = ' ')
 
+print('')
+
 #While
 
 x2 = 0
 
-while x2 != 1: #ele fica estagnado no texto do input ate o usuario digitar -1, mesmo o usuario digitando qualquer outro numero ele quebra a linha e printa novamente o input 
+while x2 != -1: #ele fica estagnado no texto do input ate o usuario digitar -1, mesmo o usuario digitando qualquer outro numero ele quebra a linha e printa novamente o input 
     x2 = float(input('Informe o -1 para sair: '))
 
 print('Fim!') #printa Fim! no console depois do usuario digitar -1
 
 
+nota = 0
+total = 0
+qtde = 0
 while nota != -1:
-    nota = float(input('Informe a nota ou -1 pra sair:')) #mesma logica do while de cima
+    nota = float(input('Informe a NOTA ou -1 pra sair:')) #mesma logica do while de cima
     if nota != -1: #enquanto o usuario nao colocar -1 e ir colocando numeros, para cada numero colocado, soma 1 na qtde, e a nota soma para o toal, para depois fazer a media
         qtde += 1
         total += nota
-
-print(f'A media da turma e {total / qtde}')
+    
+if(qtde > 0):
+    print(f'A media da turma e {total / qtde}')
 
 r = 10 #atribui 10 a r
 
@@ -296,15 +316,266 @@ for p in pessoas:
     for a in adjs: #um for dentro do outro para printar o nome e concatenar com o adjetivo
         print(f'{p} e {a}!')
         
+print(' ')
+
 for i in [1, 2, 3]:
     pass #aqui ele faz o loop mas depois nao faz nada ele so sai pois em python nao pode ter bloco vazio, entao ele roda o for mas nao executa nada
+
+print(' ')
 
 for i in range(1, 11): #um for de 1 ate o 10
     if i % 2: #se i for impar ele cai no continue pois o resto e 1 e isso significa true ai roda o continue, se nao ele print o valor de i que sempre e par pois um o resto de uma divisao por 2 de um par e 0 caindo no false e pulando o continue e printando o i
         continue
     print(i, end = ' ')
 
+print(' ')
+
 for i in range(1, 11):
     if i == 5: #ele roda o for ate o 4 e depois cai no break e sai do for
         break
     print(i)
+        
+print(' ')
+
+#SEGUNDO VIDEO
+
+#FUNCOES
+
+def saudacao(nome = 'Pessoa', idade = 20):
+    print(f'Bom dia {nome} vc nem parece ter {idade}')
+    
+saudacao() #ela printa Pessoa com a idade 20
+
+print(' ')
+
+saudacao('Jorge') #printa Jorge com a idade 20 e posso passar uma idade diferente tbm
+
+print(' ')
+
+if __name__ == '__main__': #so sera executado se o arquivo for o main
+    saudacao('Ana',20)
+
+def soma_e_multi(a, b, x):
+    return a + b * x #aqui ele realiza o b * x primeiro e depois soma com a, em C precisaria dos parenteses para ele executar a multiplicacao, aqui ja nao e necessario
+    
+#Args
+
+def soma(*nums): #criando uma funcao com um for, no qual soma todo n dentro de nums dentro da variavel total
+    total = 0
+    for n in num:
+        total += n
+    return total
+
+print(' ')
+
+f = (1, 2, 3, 4, 5, 6) #uma tupla 
+s = soma(f)    #chama a funcao acima
+print(s)    #printa 21
+
+def resultado_final(**kwargs):
+    status = 'aprovado(a)' if kwargs['nota'] >= 7 else 'reprovado'
+    return f'{kwargs["nome"]} foi {status}'
+
+resultado = resultado_final(nome = 'Pedro', nota = 7.3)
+print(resultado)
+
+#Funcional
+
+def somar(o,p): #define uma funcao de soamr 2 elementos 
+    return o + p
+
+pol = somar(2,4) #soma os elementos 2 e 4 e guarda o resultado na variavel pol
+print(pol) #printa 6
+
+def operacao_aritmetica(fn, op1, op2): #uma funcao que recebe o nome de outra funcao e mais 2 elementos que serao usados na outra funcao que ela chama
+    return fn(op1,op2)
+
+resurtado = operacao_aritmetica(somar, 12, 28) #chama a funcao somar e soma 12 e 28
+
+print(' ')
+
+print(resurtado) #printa 40
+
+def soma_parcial(a): #define uma funcao que recebe um parametro a
+    def conc_soma(b): #se tivesse algo na func soma_parcial a ser executado ele executaria e depois iria para a conc_soma e usaria o valor de a para somar com b e retornaria a ultima funcao
+        return a + b
+    return conc_soma
+
+fn = soma_parcial(10) #defina a como 10 e guarda em fn
+res_final = fn(12) #define b como 12
+
+print(' ')
+
+print(res_final)
+
+#Map Reduce
+
+from functools import reduce
+
+def mais_um_meio(nota): #soma um meio no valor passado
+    return nota + 1.5
+
+notas = [6.4, 7.2, 5.8, 8.4] #uma lista com notas do tipo float
+
+notas_finais = map(mais_um_meio, notas) #soma um meio em todas as notas, o map substitui o for e soma um meio pra cada item da lista
+
+def somar_nota(delta): #uma funcao dentro da outra igual a soma_parcial
+    def somaar(nota):
+        return nota + delta
+    return somaar
+
+notas_finais_2 = map(somar_nota(1.5), notas) #aqui a nota do indice atual vira o "b" e o 1.5 vira o "a"
+
+tootal = reduce(somar, notas, 0) #aqui ele vai comar as notas da lista comecando do 0 
+
+print(' ')
+
+print(tootal)
+
+print(' ')
+
+print(notas)
+
+#Lambdas
+
+alunos = [
+    {'nome': 'John Lennon', 'nota': 10.0},
+    {'nome': 'Taylor Swift', 'nota': 10.0},
+    {'nome': 'Conner McKnight', 'nota': 10.0},
+    {'nome': 'Kira Ford', 'nota': 2.8},
+    {'nome': 'Ethan James', 'nota': 8.5},
+]
+
+aluno_aprov = lambda aluno: aluno['nota'] >= 7 #guarda se o aluno esta aprovado se a nota dele for maior que 7
+
+alunosaprovad = list(filter(aluno_aprov, alunos)) #ele filtra para cada aluno na lista chamanda a funcao aluno_aprov e verifica se a nota e maior que 7
+
+print(' ')
+
+print(alunosaprovad) #mostra todos os alunos, menos a Kira Ford que a nota e menor que 7
+
+obter_nota = lambda aluno: aluno['nota'] #pega somente a nota do aluno
+
+print(' ')
+
+print(obter_nota(alunos[2])) #pega somente a nota do aluno no indice 2
+
+notas_aprov = list(map(obter_nota, alunosaprovad)) #mostra a nota de cada aluno aprovado
+
+print(' ')
+
+print(notas_aprov)
+
+tottal = reduce(somar, notas_aprov) #soma as notas de todos os alunos aprovados
+
+print(' ')
+
+print(tottal / len(alunosaprovad)) #pega a soma das notas e divide pela quantidade de alunos aprovados, assim pegando a media
+
+
+#Comprehension eu irei deixar toda essa parte comentada, pois ele esta alterando o codigo acima
+
+#alunosaprovad = [aluno for aluno in alunos] --> removemos o filter, usamos o [] que e uma lista
+
+#alunosaprovad = [aluno for aluno in alunos if aluno['nota'] >= 7] --> pegamos os alunos com nota maior ou igual a 7
+
+#notas_aprov = [aluno['nota'] for aluno in alunosaprovad] --> pega as notas dos alunos aprovados
+
+#CLASSE
+
+class Produto:
+    def __init__(self, nome, preco = 1.9, desc= 0 ): #define a classe produto com nome, preco e desconto, se nao passarmos valores diferentes de preco ou desc, eles continuam esses que estao definidos no parametros
+        self.nome = nome 
+        self.__preco = preco #os __ e para definir que e privado
+        self.desc = desc
+    
+    @property
+    def preco(self): #como se fosse um geter em java, retorna o preco que e uma variavel privada dentro de produto
+        return self.__preco
+    
+    @preco.setter #para ele saber que e para alterar o valor de algum produto
+    def preco(self, novo_preco):
+        if novo_preco > 0:
+            self.__preco = novo_preco
+    
+    @property #um decorador pra chamar a funcao de baixo como se fosse um atributo
+    def preco_final(self):
+        return (1 - self.desc) * self.preco
+
+
+p1 = Produto('Caneta', 10.00, 0.1) 
+p2 = Produto('Caderno', 12.99, 0.2) #cria produtos com base na classe criada
+
+p1.preco = 80.00 
+p2.preco = 27.00 #muda os valores dos precos dos itens
+
+print(p1.nome, p1.preco, p1.preco_final) 
+print(p2.nome, p2.preco, p2.preco_final) #printa nome, preco e preco final dos itens
+
+#Heranca
+
+class Carro: #cria a classe carro com 2 metodos
+    def __init__(self):
+        self.__velocidade = 0
+    
+    @property
+    def velocidade(self):
+        return self.__velocidade
+    
+    def acelerar(self):
+        self.__velocidade += 5
+        return self.__velocidade
+    
+    def frear(self):
+        self.__velocidade -= 5
+        return  self.__velocidade   
+    
+class Uno(Carro): #Uno herda as funcoes da classe carro
+    pass
+
+class Ferrari(Carro): #Ferrari herda as funcoes de carro, agora da pra chamar as funcoes mesmo usando outro nome alem de carro
+    def acelerar(self):
+        super().acelerar() 
+        return super().acelerar()
+
+c1 = Uno() 
+
+print(c1.acelerar())
+print(c1.acelerar())
+print(c1.acelerar())
+print(c1.frear())
+print(c1.frear()) 
+print(c1.frear()) #printando o acelerar e o freiar no console
+
+#Membros
+
+class Contador:
+    contador = 0 #contador recebe 0
+    
+    def inst(self): #cria uma funcao chamada inst 
+        return 'Estou bem!'
+    
+    @classmethod #define que e uma funcao da classe
+    def inc(cls):
+        cls.contador += 1 #soma um ao contador
+        return cls.contador 
+
+    @classmethod
+    def dec(cls):
+        cls.contador -= 1 #diminuiu um no contador
+        return cls.contador 
+    
+    @staticmethod #fala q e uma funcao normal
+    def mais_um(n):
+        return n + 1
+
+con1 = Contador()
+con1.inst()
+
+print(Contador.inc())
+print(Contador.inc())
+print(Contador.inc())
+print(Contador.dec())
+print(Contador.dec())
+print(Contador.dec())
+print(Contador.mais_um(99)) #printa 100
