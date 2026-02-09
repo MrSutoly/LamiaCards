e8359b4bfe3a7843dde42168729136f1a7a5e57c
João Pedro Gomes <joaopedro10092006@hotmail.com>
2026-02-09 12:29:01 -0300

    tentando fazer funcionar

---
+++ Card 8/aula/primeiro_aula.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+
+with open('aula/home_aula.html', 'r') as html_file:
+    content = html_file.read()
+    
+    soup = BeautifulSoup(content, 'lxml')
+    courses_html_tags = soup.find_all('h5')
+    for course in courses_html_tags:
+        print(course.text)
+        
+    print()
+        
+    course_card = soup.find_all('div', class_ = 'card')
+    for course in course_card:
+        course_name = course.h5.text
+        course_price = course.a.text.split()[-1]
+        
+        print(f'{course_name} cost {course_price}')
